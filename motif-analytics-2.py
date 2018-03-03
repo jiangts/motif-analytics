c8aabaa0dad7e4e5dde86f9651409651b4c2c976
ajahanso <ajahanso@stanford.edu>
2018-03-02 18:06:15 -0800

    rankings for users-by-rank-room and domains-by-rank now functional

--- motif-analytics-2.py
+++ motif-analytics-2.py
@@ -35,37 +35,41 @@ room2users = defaultdict(set)
 #attempting to aggregate data so that you can query by user id and by room
 def iterateRows(data):
 	count_int = int(0)
-	print type(count_int)
 	count = 0
+	count_int_2 = int(0)
 	for row in data:
-		#count += 1
-		count_int = decodeRow(row, count_int)
-		print count_int
+		count += 1
+		count_int, count_int_2 = decodeRow(row, count_int, count_int_2)
+	print "num add sessions: "
 	print count_int
+	print "num close room: "
+	print count_int_2
+	print "total count"
+	print count
 	return
 
 #by iterating through the data one time, we should capture all of the relevant 
 #information into a dictionary
-def decodeRow(row, count_int): 
+def decodeRow(row, count_int, count_int_2): 
 	#need to check the action before doing anything because some of the 
 	#data are missing pieces... if "open-room" there is no "room-id" yet and
 	#"origin" is only guaranteed to appear during an "add-session" call
 	userAction = row["method"]
 	if userAction == "open-room" or userAction == "join-room" or userAction == "leave-room" or userAction == "open-or-join" or userAction == "get-room":
-		return int(count_int)
+		return int(count_int), int(count_int_2)
 	roomID = row["room-id"]
 	timeStamp = row["createdAt"]
 	userID = row["user-id"]
 	if userAction == "add-session":
 		count_int += 1
-		calculateRoomTime(roomID, timeStamp, "start")
+		calculateRoomTime(roomID, timeStamp)
 		domainName = row["origin"]
 		room2domain[roomID].append(domainName)
 	#since the room is already in existence, we need to check to see 
 	#if the room is being closed
 	elif userAction == "close-room":
-		calculateRoomTime(roomID, timeStamp, "end")
-		
+		calculateRoomTime(roomID, timeStamp)
+		count_int_2 += 1
 	#generate all roomID to a list of users that are in the room
 	if roomID in room2users:
 		if userID not in room2users[roomID]:
@@ -79,13 +83,13 @@ def decodeRow(row, count_int):
 			id2room[userID].add(roomID)
 	else:
 		id2room[userID] = set([roomID])
-	
-	return int(count_int)
+	 
+	return int(count_int), int(count_int_2)
 
 #stores the room time in the 0 and 1 index of the room2report dictionary
 #at the 0 is the start time (when the room id is assigned) and at the 
 #1 is the end time of the room	
-def calculateRoomTime(roomID, timeStamp, action):
+def calculateRoomTime(roomID, timeStamp):
 	#time = timeStamp[11:13] + '.' + timeStamp[14:16]
 	#time = float(time)
 	#timeStamp example: 2018-02-28T09:04:11.310000
@@ -121,8 +125,6 @@ def graphRoom(people_per_room_dict, x_axis_label, y_axis_label, title_label):
 	plt.ylabel(y_axis_label)
 	plt.title(title_label)
 	plt.show()
-	print type(keys_list[0])
-	print type(values_list[0])
 	
 #this is the length of each row of room2report minus 2 (which are the start & end
 #times) 
@@ -143,18 +145,20 @@ def myround(x, base=5):
 #calculates the average amount of time spent in each room in question
 def timePerRoom():
 	time_list = defaultdict(int)
+	count_int = 0
 	for key, value in room2timeStamp.iteritems():
 		if len(value) > 1:
+			count_int += 1
+			#print count_int
 			d1 = min(value)
 			d2 = max(value)
-			print [d1, d2]
+			#print [d1, d2]
 			diff = d2 - d1
 			minutes = (diff.seconds) / 60
-			print (str(minutes) + ' Minutes')
+			#print (str(minutes) + ' Minutes')
 			relevantTime = minutes
 			if relevantTime > 60:
 				relevantTime = 60
-			print relevantTime
 		else:
 			relevantTime = "-inf" #there is no end time
 		if relevantTime in time_list:
@@ -162,13 +166,56 @@ def timePerRoom():
 			time_list[relevantTime] = x
 		else:
 			time_list[relevantTime] = 1
+	print "at least two timestamps" 
+	print count_int
 	return time_list
+	
+def keyWithMaxVal(d):
+    #a) create a list of the dict's keys and values; 
+    #b) return the key with the max value
+	v=list(d.values())
+	k=list(d.keys())
+	return k[v.index(max(v))]
+	
+def rankByAppearance(input, desiredLength):
+	returnList = range(desiredLength)
+	if input == "domains":
+		domains_dict = defaultdict(int)
+		#use the room2domain
+		for key, value in room2domain.iteritems():
+			for v in value:
+				if v in domains_dict:
+					s = domains_dict[v] + 1
+					domains_dict[v] = s
+				else:
+					domains_dict[v] = 1
+		for x in range(desiredLength):
+			keyToRemove = keyWithMaxVal(domains_dict)
+			returnList[x] = keyToRemove
+			domains_dict.pop(keyToRemove)
+		return returnList
+	elif input == "users":
+		users_dict = defaultdict(int)
+		for key, value in id2room.iteritems():
+			roomCount = len(value)
+			users_dict[key] = roomCount
+		for x in range(desiredLength):
+			keyToRemove = keyWithMaxVal(users_dict)
+			returnList[x] = keyToRemove
+			users_dict.pop(keyToRemove)
+		return returnList
+
+def printList(a_list):
+	for x in range(len(a_list)):
+		string_x = str((x + 1))
+		one_line =  string_x + "." + str(a_list[x])
+		print one_line
 
 def main():
 	response = requests.get(API_URL)
 	data = response.json()
-	for item in data['query_result']:
-		print item		
+	#for item in data['query_result']:
+		#print item		
 	iterateRows(data['query_result']['data']['rows'])
 	#print room2report
 	#working with args
@@ -195,13 +242,33 @@ def main():
 			print ("3. time-per-room")
 			print ('\n')
 	elif args[1] == "print":
-		"hi"
+		if len(args) > 3:
+			desiredLength = int(args[3])
+		if args[2] == "domains-by-rank":
+			a_list = rankByAppearance("domains", desiredLength)
+			printList(a_list)
+		elif args[2] == "users-by-rank-room":
+			a_list = rankByAppearance("users", desiredLength)
+			printList(a_list)
+		elif args[2] == "users-by-rank-time":
+			a_list = rankByTime()
+			printList(a_list, desiredLength)
+		else:
+			print("You did not imput a valid printing request, acceptable requests are")
+			print ('\n')
+			print ("1. domains-by-rank")
+			print ('\n')
+			print ("2. users-by-rank-room")
+			print ('\n')
+			print ("3. users-by-rank-time")
+			print ('\n')
 	return
 		
 	#command line arguments:
 	#two options: graph or print
 	#graph options: 1. users-per-room 2. domains-per-room 3. time-per-room
-	#print options: 4. domains-by-rank, 5. users-by-rank 
+	#print options: 4. domains-by-rank, 5. users-by-rank-room, 6. users-by-rank-time, optional third arg of a list of
+	#how long you want the printed list to be
         
 if __name__ == '__main__':
     main()
